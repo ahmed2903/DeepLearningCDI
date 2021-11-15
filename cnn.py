7c6acc4a9bdda4d597d94b11970a3e71c613d403
Ahmed Hussein <ahmedmo2903@gmail.com>
2021-11-15 23:17:44 +0000

    Added 2D working code that was tested on Iridis 5

---
+++ cnn.py
@@ -0,0 +1,437 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchsummary import summary
+from torch.optim.lr_scheduler import StepLR
+from torch.autograd import Variable
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from torch.utils.data import Dataset, DataLoader
+from torch.autograd import Variable
+
+import cupy as cp
+
+from torch.utils.dlpack import to_dlpack
+from torch.utils.dlpack import from_dlpack
+
+
+class double_conv(nn.Module):
+	def __init__(self, in_ch, out_ch, LRLUGrad=0.2, eps=1e-8, momentum=0.1):
+		super(double_conv, self).__init__()
+		self.conv = nn.Sequential(
+			nn.Conv2d(in_ch, out_ch, kernel_size=(3, 3), stride=1, padding=(1, 1), bias=True), # FIX ME 3D
+			#nn.Conv3d(in_ch, out_ch, kernel_size=(3, 3, 3), stride=1, padding=(1, 1, 1), bias=True),
+			nn.LeakyReLU(LRLUGrad, inplace=True),
+			nn.BatchNorm2d(num_features=out_ch, eps=eps, momentum=momentum, affine=True, track_running_stats=False), # FIX ME 3D
+			nn.Conv2d(out_ch, out_ch, kernel_size=(3, 1), stride=1, padding=(1, 0), bias=True), # FIX ME 3D
+			nn.Conv2d(out_ch, out_ch, kernel_size=(1, 3), stride=1, padding=(0, 1), bias=True), # FIX ME 3D
+			#nn.Conv3d(out_ch, out_ch, kernel_size=(3, 1, 1), stride=1, padding=(1, 0, 0), bias=True), 
+			#nn.Conv3d(out_ch, out_ch, kernel_size=(1, 3, 1), stride=1, padding=(0, 1, 0), bias=True),
+			#nn.Conv3d(out_ch, out_ch, kernel_size=(1, 1, 3), stride=1, padding=(0, 0, 1), bias=True),
+			nn.LeakyReLU(LRLUGrad, inplace=True),
+			nn.BatchNorm2d(num_features=out_ch, eps=eps, momentum=momentum, affine=True, track_running_stats=False), # FIX ME 3D
+		)
+	def forward(self, x):
+		x = self.conv(x)
+		return x
+
+
+class inconv(nn.Module):
+	def __init__(self, in_ch, out_ch, LRLUGrad=0.2, eps=1e-8, momentum=0.1):
+		super(inconv, self).__init__()
+		self.conv = nn.Sequential(
+			nn.Conv2d(in_ch, out_ch, kernel_size=(1, 1), stride=1, padding=(0, 0), bias=True), # FIX ME 3D
+			nn.LeakyReLU(LRLUGrad, inplace=False), 
+			nn.BatchNorm2d(num_features=out_ch, eps=eps, momentum=momentum, affine=True, track_running_stats=False), # FIX ME 3D
+
+			nn.Conv2d(out_ch, out_ch, kernel_size=(3, 1), stride=1, padding=(1, 0), bias=True), # FIX ME 3D
+			nn.Conv2d(out_ch, out_ch, kernel_size=(1, 3), stride=1, padding=(0, 1), bias=True), # FIX ME 3D
+			nn.LeakyReLU(LRLUGrad, inplace=True),
+			nn.BatchNorm2d(num_features=out_ch, eps=eps, momentum=momentum, affine=True, track_running_stats=False), # FIX ME 3D
+		)
+	def forward(self, x):
+		x = self.conv(x)
+		return x
+
+
+class down(nn.Module):
+	def __init__(self, in_ch, out_ch):
+		super(down, self).__init__()
+		self.mpconv = nn.Sequential(
+			nn.MaxPool2d(kernel_size=(2, 2)),
+			double_conv(in_ch, out_ch),
+		)
+	def forward(self, x):
+		x = self.mpconv(x)
+		return x
+
+
+class up(nn.Module):
+	def __init__(self, in_ch, out_ch):
+		super(up, self).__init__()
+		self.upconv = nn.Sequential(
+			nn.Upsample(scale_factor=2, mode='nearest'),
+			double_conv(in_ch, out_ch),
+		)
+	def forward(self, x):
+		x = self.upconv(x)
+		return x
+
+
+class outconv(nn.Module):
+	def __init__(self, in_ch, out_ch):
+		super(outconv, self).__init__()
+		self.conv = nn.Conv2d(in_ch, out_ch, kernel_size=(1, 1), stride=1, padding=0, bias=True)
+	def forward(self, x):
+		x = self.conv(x)
+		return x
+
+"""
+class NNModel(nn.Module):
+	def __init__(self, nchannels=1, nclasses=1, nchannels_expand=64, image_size=64):
+		super(NNModel, self).__init__()
+		self.inconv = inconv(nchannels, nchannels_expand)
+		self.down = []
+		self.up0 = []
+		self.up1 = []
+		size = image_size//2
+		f = 0
+		while size > 4:
+			self.down.append([nchannels_expand*(2**(f)), nchannels_expand*(2**(f+1))])
+			f += 1
+			size = size//2
+		# 
+		for i in range(f, 0, -1):
+			self.up0.append([nchannels_expand*(2**(i)), nchannels_expand*(2**(i-1))])
+			self.up1.append([nchannels_expand*(2**(i)), nchannels_expand*(2**(i-1))])
+		# 
+		self.up0.append([nchannels_expand, nclasses])
+		self.up1.append([nchannels_expand, nclasses])
+	def forward(self, x):
+		x = self.inconv(x)
+		for d in self.down:
+			temp = down(d[0], d[1])
+			x = temp(x)
+		x0 = x[:, 0::2, :, :] # FIX ME 3D
+		x1 = x[:, 1::2, :, :] # FIX ME 3D
+		for u in self.up0:
+			x0 = up.forward(u)
+		for u in self.up1:
+			x1 = up.forward(u)
+		x0 = torch.relu(x0)
+		x1 = torch.relu(x1)
+		# x2 = torch.clamp(x2, min=0, max=1)
+		xout = torch.cat((x0, x1), 1)
+		return xout
+"""
+	
+class NNModel(nn.Module):
+	def __init__(self, n_channels=1, n_classes=1):
+		super(NNModel, self).__init__()
+		self.inconv = inconv(n_channels, 64)
+		self.down1 = down(64, 128)
+		self.down2 = down(128, 256)
+		self.down3 = down(256, 512)
+		self.down4 = down(512, 1024)
+
+		self.up01 = up(512, 256)
+		self.up02 = up(256, 128)
+		self.up03 = up(128, 64)
+		# self.up04 = up(128, 64)
+		self.outc00 = outconv(64, n_classes)
+
+		self.up11 = up(512, 256)
+		self.up12 = up(256, 128)
+		self.up13 = up(128, 64)
+		# self.up04 = up(128, 64)
+		self.outc11 = outconv(64, n_classes)
+
+	def forward(self, x):
+		x = self.inconv(x)
+		x = self.down1(x)
+		x = self.down2(x)
+		x = self.down3(x)
+		x = self.down4(x)
+
+		x1 = x[:, 0::2, :, :]
+		x1 = self.up01(x1)
+		x1 = self.up02(x1)
+		x1 = self.up03(x1)
+		# x1 = self.up04(x, x1)
+		x1 = self.outc00(x1)
+
+		x2 = x[:, 1::2, :, :]
+		x2 = self.up11(x2)
+		x2 = self.up12(x2)
+		x2 = self.up13(x2)
+		# x = self.up4(x, x2)
+		x2 = self.outc11(x2)
+
+		x1 = torch.relu(x1)
+		x2 = torch.relu(x2)
+		# x2 = torch.clamp(x2, min=0, max=1)
+		x0 = torch.cat((x1, x2), 1)
+
+		return x0
+class CNNTrain():
+	def __init__(self, device_type='cpu'):
+		self.verbose = True
+		self.print_every = 40
+		self.device_type = device_type
+		self.device = None
+		self.input_data = None
+		self.target_data = None
+		self.data_shape = None
+		self.nchannels = 1
+		self.nclasses= 1
+		self.nchannels_expand = 64
+		self.image_size = 64
+		self.model = None
+		self.train_idx = None
+		self.test_idx = None
+		self.batch_size = 192
+		self.valid_size=0.05
+		self.loader_train = None
+		self.loader_test = None
+		self.lrate_step_size = 10
+		self.lr = 1e-2
+		self.momentum = 0.9
+		self.gamma = 0.1
+		self.optimiser = None
+		self.scheduler = None
+		self.epochs = 250
+		self.train_loss = []
+		self.valid_loss = []
+	def SetDeviceType(self, device_type='cpu'):
+		if torch.cuda.is_available() and device_type=='cuda':
+			self.device = torch.device("cuda")
+		else:
+			self.device = torch.device("cpu")
+	def SetInputData(self, fname):
+		self.input_data = np.load(fname)
+		self.data_shape = self.input_data.shape
+	def SetTargetData(self, fname):
+		self.target_data = np.load(fname)
+	def SetDimensions(self):
+		self.nchannels = self.data_shape[1]
+		self.nclasses= self.data_shape[1]
+		self.nchannels_expand = self.data_shape[-1]
+		self.image_size = self.data_shape[-1]
+	def SetModel(self, model):
+		self.model = model( 1, 1).to(self.device)
+	def SetValidSize(self, valid_size):
+		self.valid_size = valid_size
+	def SplitData(self):
+		num_train = len(self.input_data)
+		# print(num_train)
+		indices = list(range(num_train))
+		split = int(np.floor(self.valid_size * num_train))
+		np.random.shuffle(indices)
+		self.train_idx, self.test_idx = indices[split:], indices[:split]
+	def SetBatchSize(self, batch_size):
+        	self.batch_size = batch_size
+	def _LoadSplitTrain(self, index):
+		datax = self.input_data[index].astype('float32')
+		datay = self.target_data[index].astype('float32')
+		num_train = len(datax)
+		# print(num_train)
+		indices = list(range(num_train))
+		np.random.shuffle(indices)
+		from torch.utils.data.sampler import SubsetRandomSampler
+		# print(split)
+		train_idx = indices[0:]
+		train_sampler = SubsetRandomSampler(train_idx)
+		dx = torch.Tensor(datax)
+		dy = torch.Tensor(datay)
+		# dataset_tmp = Dataset(torch.Tensor(datax), torch.Tensor(datay))
+		data_xy = torch.utils.data.TensorDataset(dx, dy)
+		trainloader = torch.utils.data.DataLoader(data_xy,
+					   sampler=train_sampler, shuffle=False)
+		del datax, datay, dx, dy, data_xy
+		return trainloader
+	def LoadSplitTrain(self, loadtype='test'):
+		if loadtype == 'train':
+			self.loader_train = self._LoadSplitTrain(self.train_idx)
+		elif loadtype == 'test':
+			self.loader_test = self._LoadSplitTrain(self.test_idx)
+		else:
+			# could raise error here
+			pass
+	def SetLRStepSize(self, lrate_step_size):
+		self.lrate_step_size = lrate_step_size
+	def SetLR(self, lr):
+		self.lr = lr
+	def SetMomentum(self, momentum):
+		self.momentum = momentum
+	def SetGamma(self, gamma):
+		self.gamma = gamma
+	def SetOptimiser(self):
+		self.optimiser = optim.SGD(self.model.parameters(), lr=self.lr, momentum=self.momentum)
+	def SetScheduler(self):
+		self.scheduler = StepLR(self.optimiser, step_size=self.lrate_step_size, gamma=self.gamma)
+		
+	def pull_out_gpu_fft(self, output):
+		X = self.data_shape[-2]
+		Y= self.data_shape[-1]
+		X2 = X//2
+		X4 = X//4
+		Y2 = Y//2
+		Y4 = Y//4
+		dx = to_dlpack(output) # type/tensor conversion
+		cx = cp.fromDlpack(dx) # type/tensor conversion
+		cx1, cx2 = cx[:, 0, :, :], cx[:, 1, :, :] #split channels 
+
+		cx11 = cp.zeros(([cx1.shape[0], 1, X, Y]), dtype='float32') #empty arrays
+		cx22 = cp.zeros(([cx2.shape[0], 1, X, Y]), dtype='float32') #empty arrays
+
+		#increase dimensions to 64x64 (input dimensions for FT)
+
+		cx11[:, 0, X4:X4+X2, Y4:Y4+Y2] = cx1.copy() # cenre array of 32x32 in array of 64x64
+		cx22[:, 0, X4:X4+X2, Y4:Y4+Y2] = cx2.copy() # cenre array of 32x32 in array of 64x64
+
+		cx1122 = cx11 * cp.exp(4j * cp.pi * cx22)
+
+		cx1122 = cp.abs(cp.fft.ifftshift(cp.fft.fft2(cp.fft.fftshift(cx1122, axes=(-2, -1)), axes=(-2, -1)), axes=(-2, -1)))
+
+		return from_dlpack(cx1122.toDlpack())
+
+
+	def pcc_loss(self, output, target):
+		x = output
+		y = target
+		vx = x - torch.mean(x)
+		vy = y - torch.mean(y)
+		loss = torch.sum(vx * vy) / (torch.sqrt(torch.sum(vx ** 2) * torch.sum(vy ** 2)))
+		return 1 - loss
+	
+	def all_loss(self, output, target, input, device):
+		X = self.data_shape[-2]
+		Y= self.data_shape[-1]
+		X2 = X//2
+		X4 = X//4
+		Y2 = Y//2
+		Y4 = Y//4
+		output0 = torch.zeros(output.shape[0], output.shape[1], X, Y, requires_grad=True).to(device)
+		output0[:, :, (X2 - X4):(X2 + X4), (Y2 - Y4):(Y2 + Y4)] = output
+		target0 = torch.zeros(output.shape[0], output.shape[1], X, Y, requires_grad=True).to(device)
+		target0[:, :, (X2 - X4):(X2 + X4), (Y2 - Y4):(Y2 + Y4)] = target
+		loss1 = torch.sqrt(torch.mean((output0[:, 0, :, :] - target0[:, 0, :, :]) ** 2) /
+						   (torch.mean(target0[:, 0, :, :] ** 2)))
+		loss2 = torch.sqrt(torch.mean((output0[:, 1, :, :] - target0[:, 1, :, :]) ** 2) /
+						   (torch.mean(target0[:, 1, :, :] ** 2)))
+		tx01 = self.pull_out_gpu_fft(output)
+		tx02 = self.pull_out_gpu_fft(target)
+		loss3 = self.pcc_loss(tx01, tx02)
+		alpha, beta, gamma = 1., 1., 4.
+		loss = (alpha * loss1 + beta * loss2 + gamma * loss3) / (alpha + beta + gamma)
+		return loss
+		
+	def criterion(self, output, target, input, device):
+		return self.all_loss(output, target, input, self.device)
+
+	def get_lr(self):
+		for param_group in self.optimiser.param_groups:
+			return param_group['lr']
+
+	def SetNEpochs(self, epochs):
+		self.epochs = epochs
+	def TrainNN(self):
+		for epoch in range(self.epochs):  # loop over the dataset multiple times
+			train_loss_tmp = 0.0
+			for ii, loader_batch_train in enumerate(self.loader_train, 0):
+				# get the inputs; data is a list of [inputs, labels]
+				x_train, y_train = loader_batch_train
+				x_train, y_train = Variable(x_train).to(self.device), Variable(y_train).to(self.device)
+				# sets all the gradients to zero; to avoid accumulation of gradients from the previous epoch
+				self.optimiser.zero_grad()
+				# forward propagation
+				y_train_predict = self.model.forward(x_train)
+				#define the loss and then backward propagate
+
+				loss1 = self.criterion(y_train_predict, y_train, x_train, self.device) #EDIT ME!
+				
+				loss1.backward()
+				#optimize the weights and biases
+				self.optimiser.step()
+
+				train_loss_tmp += loss1.item()
+				
+				# print info if needed
+				if self.verbose:
+					if ii % self.print_every == 0:  # print every 2000 mini-batches
+						print('[%d, %5d] Batch loss:: train %.5f'%(epoch + 1, ii + 1, train_loss_tmp / (ii + 1)))
+
+			# turn off some specific parts of the model for the evaluation with model.eval()
+			self.model.eval()
+			#validation step
+			with torch.no_grad():
+				valid_loss_tmp = 0.0
+				for jj, loader_batch_test in enumerate(self.loader_test, 0):
+					x_test, y_test = loader_batch_test
+					x_test, y_test = x_test.to(self.device), y_test.to(self.device)
+					y_pred = self.model.forward(x_test)
+					loss2 = self.criterion(y_pred, y_test, x_test, self.device) # EDIT ME!!
+					valid_loss_tmp += loss2.item()
+
+		#update the learning rate
+		self.scheduler.step()
+		self.train_loss.append(train_loss_tmp / len(self.loader_train))
+		self.valid_loss.append(valid_loss_tmp / len(self.loader_test))
+
+		# if ii % print_every == 0:                  # print every mini-batches
+		if self.verbose:
+			print('Epoch-loss:: train %.5f  valid %.5f lr %.5f' %(self.train_loss[-1], self.valid_loss[-1], self.get_lr())) #FIXME get_lr
+		
+		# save
+		self.SaveModel(epoch)
+	##
+	def SaveModel(self, epoch=0):
+		torch.save(self.model.state_dict(), 'checkpoint/' + 'CP{}.pth'.format(epoch+1))
+	def PlotLoss(self):
+		# plot the validation loss
+		plt.plot(self.train_loss, label='Training loss')
+		plt.plot(self.valid_loss, label='Validation loss')
+		plt.legend(frameon=False)
+		plt.savefig('validation_error.png')
+		plt.show()
+			
+	def SaveParameters(self):
+		params = ""
+		params += "Device Type: %s \n"%self.device_type
+		params += "Validation Size: %1.5e  \n"%self.valid_size
+		params += "Batch Size: %2.6f \n" %self.batch_size
+		params += "Learning Rate: %2.6f \n" %self.lr
+		params += "Learning Rate Step Size: %2.6f \n" %self.lrate_step_size
+		params += "Momentum: %d \n" %self.momentum
+		params += "Gamma: %d \n" %self.gamma
+		params += "Number of Epochs: %d \n" %self.epochs
+
+		f = open('CNN_Training_Params.txt', "w")
+		f.write(params)
+		f.close()
+
+
+if __name__ == '__main__':
+	mynn = CNNTrain()
+	mynn.SetDeviceType('cuda')
+	mynn.SetInputData('reci_intes.npy')
+	mynn.SetTargetData('real_obj.npy')
+	mynn.SetModel(NNModel)
+	mynn.SetValidSize(0.05)
+	mynn.SplitData()
+	mynn.SetBatchSize(192)
+	mynn.LoadSplitTrain(loadtype='train')
+	mynn.LoadSplitTrain(loadtype='test')
+	mynn.SetLRStepSize(10)
+	mynn.SetLR(1e-2)
+	mynn.SetMomentum(0.9)
+	mynn.SetGamma(0.1)
+	mynn.SetOptimiser()
+	mynn.SetScheduler()
+	mynn.SetNEpochs(250)
+	mynn.TrainNN()
+	mynn.PlotLoss()
+	mynn.SaveParameters()
